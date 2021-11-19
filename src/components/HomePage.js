0ae01b25857aa513e18f807d292c9162c69fb0fc
Jared Drake <jareddrake@Jareds-MacBook-Pro.local>
2021-11-19 15:05:44 -0700

    add animation to divs still needs work

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./HomePage.css";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 const HomePage = () => {
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
+
   return (
     <div className='home-page'>
       <div className='home-photo-container'>
@@ -21,13 +27,13 @@ const HomePage = () => {
       <div className='main-info'>
         <h1>Move with us</h1>
         <div className='card-one-info'>
-          <div className='card-one-picture'>
+          <div data-aos='fade-right' className='card-one-picture'>
             <img
               src='https://media.istockphoto.com/photos/happy-manual-worker-unloading-cardboard-boxes-from-delivery-van-picture-id1186578902?b=1&k=20&m=1186578902&s=170667a&w=0&h=pz0WCWnRGVAKMLRYley3cLxzENzs6e5bxD2tSOqn1yc='
               alt=''
             />
           </div>
-          <div className='card-one-text'>
+          <div data-aos='fade-left' className='card-one-text'>
             <h3>Hourly Rates</h3>
             <h5>$125 For two men and a truck</h5>
             <h5>$175 For 3 man and a truck</h5>
@@ -36,13 +42,13 @@ const HomePage = () => {
           </div>
         </div>
         <div className='card-two-info'>
-          <div className='card-two-picture'>
+          <div data-aos='fade-right' className='card-two-picture'>
             <img
               src='https://media.istockphoto.com/photos/its-moving-day-picture-id1147468408?b=1&k=20&m=1147468408&s=170667a&w=0&h=UPh1xXoW3fIlwdhFcIQv9rOPMsjwDwNsnuKaGKUob8k='
               alt=''
             />
           </div>
-          <div className='card-two-text'>
+          <div data-aos='fade-left' className='card-two-text'>
             <h3>Packing Services</h3>
             <h5>Single Item Moves</h5>
             <h5>Piano Moves</h5>
@@ -52,13 +58,13 @@ const HomePage = () => {
           </div>
         </div>
         <div className='card-three-info'>
-          <div className='card-three-picture'>
+          <div data-aos='fade-right' className='card-three-picture'>
             <img
               src='https://images.pexels.com/photos/7464643/pexels-photo-7464643.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'
               alt=''
             />
           </div>
-          <div className='card-three-text'>
+          <div data-aos='fade-left' className='card-three-text'>
             <h3>Out of State Moves</h3>
             <h5>Call, Email, or Text for quote</h5>
           </div>
