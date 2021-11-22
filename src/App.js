d0a620d6f5f18810b3eeeee70f8d57a77e85d39a
Jared Drake <jareddrake@Jareds-MacBook-Pro.local>
2021-11-22 06:29:24 -0700

    added footer still needs work and need more info from him

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
 import HomePage from "./components/HomePage";
+import Footer from "./components/Footer";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <div className='App-header'>
-        <NavBar />
-        <HomePage />
-      </div>
-    </Router>
+    <div className='App'>
+      <NavBar />
+      <HomePage />
+      <Footer />
+    </div>
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import { InstagramFilled } from "antd";
+import Icon from "@mdi/react";
+import { mdiInstagram, mdiFacebook } from "@mdi/js";
+
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className='Footer'>
+      <hr />
+      <div class='container'>
+        <div className='social-media-links'>
+          <div className='instagram'>
+            <Icon path={mdiInstagram} title='User Profile' size={2} />
+          </div>
+          <div className='facebook'>
+            <Icon path={mdiFacebook} title='User Profile' size={2} />
+          </div>
+        </div>
+      </div>
+      <hr />
+    </footer>
+  );
+};
+export default Footer;
