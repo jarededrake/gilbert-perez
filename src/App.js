0ee25411bd09abdacecc2c0943f65d79549a02c2
Jared Drake <jareddrake@Jareds-MBP.lan>
2021-11-26 12:15:16 -0700

    adding routing and linking

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import "./App.css";
 import NavBar from "./components/NavBar";
 import HomePage from "./components/HomePage";
 import Footer from "./components/Footer";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import ContactUsPage from "./components/ContactUsPage";
+import QuotePage from "./components/QuotePage";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link,
+  Switch,
+  BrowserRouter,
+} from "react-router-dom";
 function App() {
   return (
     <div className='App'>
       <NavBar />
-      <HomePage />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='quote' element={<QuotePage />} />
+        <Route path='contact' element={<ContactUsPage />} />
+      </Routes>
       <Footer />
     </div>
   );
---
+++ src/components/QuotePage.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+const QuotePage = () => {
+  return (
+    <div className='quote'>
+      <h1>This is the quote page</h1>
+    </div>
+  );
+};
+
+export default QuotePage;
