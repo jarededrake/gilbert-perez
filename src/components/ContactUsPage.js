b80c02d661e692563da6d44e65ebdf9fb400c898
Jared Drake <jareddrake@Jareds-MBP.lan>
2021-11-26 23:04:20 -0700

    added styling to contact us and faq page

--- src/components/ContactUsPage.js
+++ src/components/ContactUsPage.js
@@ -12,6 +12,67 @@ const ContactUsPage = () => {
         </p>
         <p>If this is an emergency please call us instad at 303-123-4567</p>
       </div>
+      <div className='contact-us-form'>
+        <form>
+          <div class='form-group'>
+            <label for='inputEmail'>Email address (required)</label>
+            <input
+              type='email'
+              class='form-control'
+              id='inputEmail'
+              aria-describedby='emailHelp'
+              placeholder='Enter email'
+            />
+          </div>
+          <div class='form-group'>
+            <label for='inputFirstName'>Your first name (required)</label>
+            <input
+              type='email'
+              class='form-control'
+              id='inputLastName'
+              aria-describedby='firstName'
+              placeholder='Enter first name'
+            />
+          </div>
+          <div class='form-group'>
+            <label for='inputLastName'>Your last name (required)</label>
+            <input
+              type='email'
+              class='form-control'
+              id='inputLastName'
+              aria-describedby='lastName'
+              placeholder='Enter last name'
+            />
+          </div>
+          <div class='form-group'>
+            <label for='inputMobilePhone'>Your mobile phone</label>
+            <input
+              type='email'
+              class='form-control'
+              id='inputMobilePhone'
+              aria-describedby='mobilePhone'
+              placeholder='Enter mobile phone'
+            />
+          </div>
+          <div class='form-group'>
+            <label for='inputSubject'>Subject</label>
+            <input
+              type='email'
+              class='form-control'
+              id='inputSubject'
+              aria-describedby='subject'
+              placeholder='Enter subject'
+            />
+          </div>
+          <div class='form-group'>
+            <label for='message'>Your message</label>
+            <textarea class='form-control' rows='5' id='message'></textarea>
+          </div>
+          <button type='submit' class='btn btn-primary'>
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
--- src/components/FaqPage.js
+++ src/components/FaqPage.js
@@ -1,9 +1,75 @@
 import React, { Component } from "react";
+import "./FaqPage.css";
 
 const FaqPage = () => {
   return (
     <div className='Faq'>
-      <h1>This is the FAQ page</h1>
+      <div className='faq-info'>
+        <h4>Frequently Asked Questions</h4>
+      </div>
+      <div className='faq-card'>
+        <div className='faq-card-question'>
+          <div className='question'>
+            <h1>Q.</h1>
+          </div>
+          <div className='question-text'>
+            <h6>How do you avoid moving damages?</h6>
+          </div>
+        </div>
+        <div className='faq-card-answer'>
+          <div className='answer'>
+            <h1>A.</h1>
+          </div>
+          <div className='answer-text'>
+            <h6>
+              Before anything is moved we wrap all of your furniture in moving
+              blankets
+            </h6>
+          </div>
+        </div>
+      </div>
+      <div className='faq-card'>
+        <div className='faq-card-question'>
+          <div className='question'>
+            <h1>Q.</h1>
+          </div>
+          <div className='question-text'>
+            <h6>Will inclement weather delay my shipment?</h6>
+          </div>
+        </div>
+        <div className='faq-card-answer'>
+          <div className='answer'>
+            <h1>A.</h1>
+          </div>
+          <div className='answer-text'>
+            <h6>
+              The only time you shipment will be delayed due to inclement
+              weather is if it is unsafe to drive in such as a white out
+              blizzard.
+            </h6>
+          </div>
+        </div>
+      </div>
+      <div className='faq-card'>
+        <div className='faq-card-question'>
+          <div className='question'>
+            <h1>Q.</h1>
+          </div>
+          <div className='question-text'>
+            <h6>Do you offer packing services?</h6>
+          </div>
+        </div>
+        <div className='faq-card-answer'>
+          <div className='answer'>
+            <h1>A.</h1>
+          </div>
+          <div className='answer-text'>
+            <h6>
+              Absoluely we do and we pack your stuff as though its our own.
+            </h6>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
