2609e9e49e92e1e651c389ac9beaddedb179d3e3
Jared Drake <jareddrake@Jareds-MBP.lan>
2021-11-26 20:32:14 -0700

    fix merge

---
+++ src/components/AboutPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+const AboutPage = () => {
+  return (
+    <div className='About'>
+      <h1>This is the about page</h1>
+    </div>
+  );
+};
+
+export default AboutPage;
--- src/components/ContactUsPage.js
+++ src/components/ContactUsPage.js
@@ -1,9 +1,17 @@
 import React, { Component } from "react";
+import "./ContactUsPage.css";
 
 const ContactUsPage = () => {
   return (
     <div className='contact-us'>
-      <h1>This is the contact us page</h1>
+      <div className='contact-us-info'>
+        <p>Please feel free to send us e-mail with any questions you have.</p>
+        <p>
+          We know that time is of the essence with your move so we strive to
+          respond before 24 hours.
+        </p>
+        <p>If this is an emergency please call us instad at 303-123-4567</p>
+      </div>
     </div>
   );
 };
